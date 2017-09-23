09f7235398fa13d40fd2c951a1b2b1c0768349ae
ria kedia <rkedia58@gmail.com>
2017-09-23 15:21:02 +0530

    Add files via upload

---
+++ fuzzy.py
@@ -0,0 +1,3 @@
+from fuzzywuzzy import fuzz
+from fuzzywuzzy import process
+print(fuzz.ratio("communication is the passing of information,data,ideas,facts","communication is the exchange of information,data,ideas,facts"))
